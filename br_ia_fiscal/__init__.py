3c90719d2affd552d2394833305bb06998a2888a
Ivan.py <146444349+martinsivan@users.noreply.github.com>
2025-11-16 13:58:23 -0300

    Create __init__.py

---
+++ br_ia_fiscal/__init__.py
@@ -0,0 +1,13 @@
+__version__ = "0.1.0"
+__author__ = "Ivan Martins"
+__description__ = "IA para conformidade fiscal brasileira"
+
+from .core.validator import NFeValidator
+
+__all__ = ["NFeValidator"]__version__ = "0.1.0"
+__author__ = "Ivan Martins"
+__description__ = "IA para conformidade fiscal brasileira"
+
+from .core.validator import NFeValidator
+
+__all__ = ["NFeValidator"]
