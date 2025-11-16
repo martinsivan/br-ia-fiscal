7b693d3f4cc2f6ccd6de0bfdecfedbc28990cc55
Ivan.py <146444349+martinsivan@users.noreply.github.com>
2025-11-16 14:21:19 -0300

    Update __init__.py

--- br_ia_fiscal/__init__.py
+++ br_ia_fiscal/__init__.py
@@ -1,13 +1,6 @@
+from .core.validator import NFeValidator
+from .core.optimizer import TaxOptimizer
+from .core.monitor import LegalMonitor
+
 __version__ = "0.1.0"
-__author__ = "Ivan Martins"
-__description__ = "IA para conformidade fiscal brasileira"
-
-from .core.validator import NFeValidator
-
-__all__ = ["NFeValidator"]__version__ = "0.1.0"
-__author__ = "Ivan Martins"
-__description__ = "IA para conformidade fiscal brasileira"
-
-from .core.validator import NFeValidator
-
-__all__ = ["NFeValidator"]
+__all__ = ["NFeValidator", "TaxOptimizer", "LegalMonitor"]
